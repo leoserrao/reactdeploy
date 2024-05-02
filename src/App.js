b87125ca64a9160a1c14825e833e3607580902c1
Léo Serrão <leo_serrao@hotmail.com>
2024-05-01 23:33:37 -0300

    commit landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <div className='center'>
+          <div className='menu'>
+            <div className='logo'>
+              <h1>My Landing Page</h1>
+            </div>
+            <div className='item-menu'>
+              <a href='https://www.smarttreine.com.br/'>Login</a>
+            </div>
+  
+          </div>
+          <div className='form'>
+              <h2>Entre em contato:</h2>
+              <form>
+                <div className='items-form'>
+                  <input type='text' placeholder='Seu nome...' />
+                  <input type='text' placeholder='Seu e-mail...'/>
+                  <input type='text' placeholder='Seu WhatsApp...' />
+                  <input type='text' placeholder='Seu interesse...' />
+                  <input type='submit' />
+                </div>
+              </form>
+            </div>
+        </div>
+
+      </div>
+      <div className='container-cards'>
+        <div className='center'>
+          <div className='conteudo-single'>
+            <h3>Título da chamada</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Título da chamada</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Título da chamada</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Título da chamada</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
